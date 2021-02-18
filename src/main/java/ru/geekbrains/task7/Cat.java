a0e9ea03b6790d13df58744faeea1c9a28958a59
Kotlyarov Kirill <zkp23@mail.ru>
2021-02-18 23:06:23 +0300

    stage

--- src/main/java/ru/geekbrains/task7/Cat.java
+++ src/main/java/ru/geekbrains/task7/Cat.java
@@ -1,2 +1,25 @@
-package ru.geekbrains.task7;public class Cat {
+package ru.geekbrains.task7;
+
+public class Cat {
+
+    private String name;
+    private int appetite;
+
+    private boolean satiety;
+
+    public Cat(String name, int appetite) {
+        this.name = name;
+        this.appetite = appetite;
+
+    }
+
+    public void eat(Plate plate) {
+        satiety = plate.decreaseFood(appetite);
+    }
+
+    @Override
+    public String toString() {
+        return "Cat{name='" + name + '\'' + ", appetite=" + appetite + ", satiety=" + satiety +'}';
+    }
+
 }
--- src/main/java/ru/geekbrains/task7/Plate.java
+++ src/main/java/ru/geekbrains/task7/Plate.java
@@ -1,2 +1,31 @@
-package ru.geekbrains.task7;public class Plate {
+package ru.geekbrains.task7;
+
+public class Plate {
+    private int foodValue;
+
+    public Plate(int foodValue){
+        this.foodValue = foodValue;
+    }
+
+    public boolean decreaseFood(int countEatenFood) {
+        if (hasEnoughFood(countEatenFood)) {
+            foodValue -= countEatenFood;
+            return true;
+        }
+        return false;
+    }
+
+    private boolean hasEnoughFood(int requiredCountFood) {
+        return foodValue >= requiredCountFood;
+    }
+
+    public void info() {
+        System.out.println("в тарелке:" + foodValue);
+    }
+
+    public void addFood(int countFood) {
+        foodValue += countFood;
+    }
+
+
 }
